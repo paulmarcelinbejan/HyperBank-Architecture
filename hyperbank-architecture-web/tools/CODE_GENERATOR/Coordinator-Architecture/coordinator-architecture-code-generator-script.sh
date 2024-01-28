60f2b50536e01aac1745d21eb10f2d25990b5183
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2024-01-28 01:30:24 +0100

    fix: code generator script

--- hyperbank-architecture-web/tools/CODE_GENERATOR/Coordinator-Architecture/coordinator-architecture-code-generator-script.sh
+++ hyperbank-architecture-web/tools/CODE_GENERATOR/Coordinator-Architecture/coordinator-architecture-code-generator-script.sh
@@ -4,42 +4,37 @@
 GREEN='\033[0;92m'
 BLUE='\033[1;94m'
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 COLOR_OFF='\033[0m'
 
-readInputVariables() {
-    if [ $# -eq 5 ]; then
-        readInputVariablesFromArguments
-    else
-        readInputVariablesFromInput
-    fi
-
-    echo
-}
-
-readInputVariablesFromArguments() {
-    # Read input variables
-    TEMPLATES_DIRECTORY="$1"
-    ENTITY="$2"
-    PACKAGE="$3"
-    ID_TYPE="$4"
-    OUTPUT_DIRECTORY="$5"
+printHorizontalLine() {
+    echo -e "$YELLOW-------------------------------------------------------------------------------------------------------- $COLOR_OFF"
 }
 
 readInputVariablesFromInput() {
-    # Read input variables
-    read -rp "Enter TEMPLATES DIRECTORY: " TEMPLATES_DIRECTORY
-    read -rp "Enter ENTITY: " ENTITY
-    read -rp "Enter PACKAGE: " PACKAGE
-    read -rp "Enter ID_TYPE: " ID_TYPE
-    read -rp "Enter PROJECT DIRECTORY: " OUTPUT_DIRECTORY
+    echo -e "${YELLOW}Enter TEMPLATES DIRECTORY: ${COLOR_OFF}"
+    read TEMPLATES_DIRECTORY
+    echo -e "${YELLOW}Enter ENTITY: ${COLOR_OFF}"
+    read ENTITY
+    echo -e "${YELLOW}Enter PACKAGE: ${COLOR_OFF}"
+    read PACKAGE
+    echo -e "${YELLOW}Enter ID_TYPE: ${COLOR_OFF}"
+    read ID_TYPE
+    echo -e "${YELLOW}Enter PROJECT DIRECTORY: ${COLOR_OFF}"
+    read PROJECT_DIRECTORY
+
+    echo
+    printHorizontalLine
+    echo
+
 }
 
 createEntityVariants() {
     # Create variants of Entity to use for package, type, variable,
     ENTITY_LOWERCASE=${ENTITY:l}
     #ENTITY_UPPERCASE=${ENTITY:u}
-    local entity_first_char=${ENTITY[1,1]}
-    local entity_rest_chars=${ENTITY[2,-1]}
+    local entity_first_char=${ENTITY[1, 1]}
+    local entity_rest_chars=${ENTITY[2, -1]}
     ENTITY_LOWERCAMELCASE="${entity_first_char:l}${entity_rest_chars}"
 }
 
@@ -53,13 +48,13 @@ replacePlaceholders() {
         -e "s/\${ENTITY}/${ENTITY}/g" \
         -e "s/\${ENTITY_LOWERCASE}/${ENTITY_LOWERCASE}/g" \
         -e "s/\${ENTITY_LOWERCAMELCASE}/${ENTITY_LOWERCAMELCASE}/g" \
-        ${from} > ${to}
+        ${from} >${to}
 }
 
 generate_impl() {
     local source_dir="$1"
     local target_dir="$2"
-    
+
     # Create the target directory if it doesn't exist
     mkdir -p "$target_dir"
 
@@ -104,11 +99,13 @@ generate_impl() {
 
         # Check if the sed command was executed correctly
         if [ $? -eq 0 ]; then
-          echo -e "$GREEN ${target_item} created correctly $COLOR_OFF"
+            echo -e "$GREEN $(basename "$target_item") created correctly! $COLOR_OFF"
         else
-          echo -e "$RED Error creating ${target_item} from ${source_item} $COLOR_OFF"
+            echo -e "$RED Error creating $(basename "$target_item") from ${source_item} !!! $COLOR_OFF"
         fi
-        
+
+        echo
+        printHorizontalLine
         echo
 
     done
@@ -126,6 +123,6 @@ readInputVariables
 createEntityVariants
 
 # Start processing from the TEMPLATES_DIRECTORY
-generate_impl "$TEMPLATES_DIRECTORY/template" "$OUTPUT_DIRECTORY/$ENTITY_LOWERCASE"
+generate_impl "$TEMPLATES_DIRECTORY/template" "$PROJECT_DIRECTORY/$ENTITY_LOWERCASE"
 
 logCodeGeneratorCompleted
